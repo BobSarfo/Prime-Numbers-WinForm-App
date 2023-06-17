aa8f942a1b9f1a358d5981f13dfb06f725e92b7b
BobSarfo <robert@phlo.io>
2023-06-17 00:27:43 +0000

    add global exceptions handle

--- PrimeNumbersApp/Program.cs
+++ PrimeNumbersApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,32 @@ namespace PrimeNumbersApp
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += ApplicationOnThreadException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            String errorMessage = String.Format("Sorry, something went wrong. \r\n" +
+                 "\r\n Please report error" +
+                  "\n\r" + ((Exception) e.ExceptionObject).Message);
+            Console.WriteLine($"Error {0} {1}", DateTimeOffset.Now,e.ExceptionObject);
+            MessageBox.Show(errorMessage, "Unexpected Error Occurred");
+        }
+
+        private static void ApplicationOnThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            String errorMessage = String.Format("Sorry, something went wrong. \r\n" +
+                "\r\n Please report error"+
+                "\n\r" + (e.Exception.Message));
+            Console.WriteLine($"Error {0} {1}",DateTimeOffset.Now, e.Exception);
+            MessageBox.Show(errorMessage,"Unexpected Error Occurred");
         }
     }
 }
